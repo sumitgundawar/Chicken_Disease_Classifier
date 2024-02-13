d47b5d00076f3b5ed5673c7aca136f77738d658d
sumitgundawar <sumitgundawar3@gmail.com>
2024-02-13 16:07:34 +0000

    Template folder added

--- template.py
+++ template.py
@@ -16,6 +16,7 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
+    "templates/index.html",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
